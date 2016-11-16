700b1826bf0951eea4c040f112c9f326cd9b6c91
mihai.frunzeti <frunzeti_m@yahoo.com>
2016-11-16 20:14:58 +0200

    l

--- Proiect1/src/Test.java
+++ Proiect1/src/Test.java
@@ -8,10 +8,12 @@ public class Test {
                     //suprascrierea metodei
 	lucru l2 = new lucru(){
 		public double calcullucru(int x, int y){
-			return 500;
+			return x+y;
 		}
 	};
 	m((d,e)->d+e,10,20);
+	m(l2,10,20);
+	
 	}
 	public static void m(lucru l, int a, int b){
 		System.out.println(l.calcullucru(a, b));
